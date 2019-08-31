3643e3d08d38a3c8084eab05bc28bce0a266dd52
Joao <ritterjv@gmail.com>
2019-08-31 14:14:49 -0700

    -

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const app = express();
 const hydrate = require("@sustainer-network/event-store-hydrate-service");
 const tokensFromReq = require("@sustainer-network/tokens-from-req");
 const middleware = require("@sustainer-network/event-store-middleware");
+const logger = require("@sustainer-network/logger");
 
 middleware(app);
 
 app.get("/", (req, res) => {
   hydrate({ params: req.query, token: tokensFromReq(req) })
     .then(aggregateRoot => res.send(aggregateRoot))
-    .catch(e => res.status(e.statusCode || 500).send(e));
+    .catch(e => {
+      logger.error("eee: ", { e, stack: e.stack });
+      res.status(e.statusCode || 500).send(e);
+    });
 });
 
 module.exports = app;
